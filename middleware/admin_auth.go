534c5c3fcef8b657513cfe76a085b57a8aa308a4
shuimengzhi <shuimengzhi@gmail.com>
2023-08-31 16:50:18 +0800

    添加中间件:管理员才能操作

--- middleware/admin_auth.go
+++ middleware/admin_auth.go
@@ -1,9 +1,64 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"runtime"
+	"strconv"
+	"taskm/cache"
+	"taskm/enum"
+	commoniostruct "taskm/io_struct/common"
+	useriostruct "taskm/io_struct/user"
+	"taskm/model"
+	"time"
+)
 
 func AdminAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
+
+		userInfo, err := cache.Instance.Get(context.Background(), token).Result()
+		if err != nil {
+			switch {
+			case err == redis.Nil:
+				c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: "登陆失败"})
+			case err != nil:
+				c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: err.Error()})
+			case userInfo == "":
+				c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: "登陆失败"})
+			}
+			c.Abort()
+			return
+		}
+		var resp useriostruct.LoginResponse
+		err = json.Unmarshal([]byte(userInfo), &resp)
+		if err != nil {
+			c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: err.Error()})
+			c.Abort()
+			return
+		}
+		if resp.RoleType != model.USER_ROLE_TYPE_ADMIN {
+			c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: "管理员才能操作"})
+			c.Abort()
+			return
+		}
+		//请求就重新计算过期时间
+		setErr := cache.Instance.Set(context.Background(), token, userInfo, 2*time.Hour).Err()
+		if setErr != nil {
+			_, file, line, ok := runtime.Caller(0)
+			if ok {
+				c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: setErr.Error() + "  " + file + ":" + strconv.Itoa(line)})
+				c.Abort()
+				return
+			}
+			c.JSON(200, commoniostruct.Response{Code: enum.CodeParamErr, Msg: setErr.Error()})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_info", &resp)
 		c.Next()
 		return
 	}
